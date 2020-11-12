77e2c577b8b51ccf053ae1d676294d7591874a70
Catalan Renegado <jciscent@gmail.com>
2020-11-12 23:26:25 +0100

    He hecho algunos testeos con las UIS

--- Utils.py
+++ Utils.py
@@ -1,29 +1,34 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QTextEdit
+from PyQt5.QtCore import Qt, QObject
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QTextEdit, QHBoxLayout, QFrame
 
 
 class KeyValueWidget(QWidget):
     def __init__(self, key: str, value: str):
         super().__init__()
-        self.main_layout = QVBoxLayout()
-        self._key: QLabel = QLabel(key)
+        self.main_layout = QHBoxLayout()
+        self.main_layout.setAlignment(Qt.AlignCenter)
+        self.key: QLabel = QLabel(key)
         self._value: QLabel = QLabel(value)
+        self.key.setFrameStyle(QFrame.Panel | QFrame.Raised)
+        self.value.setFrameStyle(QFrame.Panel | QFrame.Raised)
         self.main_layout.addWidget(self.key)
         self.main_layout.addWidget(self.value)
+        self.setLayout(self.main_layout)
 
     @property
-    def key(self):
-        return self._key.text()
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self,value):
+        if isinstance(value, QObject):
+            self.main_layout = QHBoxLayout()
+            self._value = value
+            self.main_layout.addWidget(self.key)
+            self.main_layout.addWidget(value)
+            self.setLayout(self.main_layout)
 
-    @key.setter
-    def key(self, value):
-        if isinstance(value,str):
-            self._key.setText(value)
-        elif isinstance(value,QLabel):
-            self._key = value
-        else:
-            raise RuntimeError()
 
 class KeyValueMutable(KeyValueWidget):
     def __init__(self, key: str, is_hidden: bool):
-        super().__init__(key)
-        self._value: QTextEdit = QTextEdit()
+        super().__init__(key, "")
+        self.value: QTextEdit = QTextEdit("b")
