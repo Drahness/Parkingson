3037d39d232265d13aeb596798e72ea300555974
Catalan Renegado <jciscent@gmail.com>
2020-12-08 16:56:41 +0100

    He actualizado requirements.txt para que no me tire a mi error el numpy, he formateado los archivos Graph.py y Prueba.py y por ultimo he cambiado algunos nombres de variables y classes dentro de Prueba.py, no he cambiado la logica en absoluto.

--- Prueba.py
+++ Prueba.py
@@ -6,12 +6,10 @@ from PyQt5.QtWidgets import (
     QWidget, QApplication, QMainWindow, QVBoxLayout,
     QHBoxLayout, QPushButton, QSizePolicy, QTabWidget
 )
-
-from Graph import MplCanvas
+from Graph import MplCanvas  # Si se te instala numpy 1.19.4 puede tirar error en windows.
 
 
 class Color(QWidget):
-
     def __init__(self, color):
         super().__init__()
         self.setAutoFillBackground(True)
@@ -24,15 +22,21 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("CronometroOrtsLayouts")
-        self.chronometer_widget = ChronometerWidget(self)
-        #sc = MplCanvas(self, width=5, height=4, dpi=100)
-        #sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
+        self.chronometer_widget = CentralWidgetParkingson(self)
+        # sc = MplCanvas(self, width=5, height=4, dpi=100)
+        # sc.axes.plot([0,1,2,3,4], [10,1,20,3,40])
 
         self.setCentralWidget(self.chronometer_widget)
         self.show()
 
 
-class ChronometerWidget(QWidget):
+class CentralWidgetParkingson(QWidget):
+    pacients_tab: QWidget  # Una vez seleccionado paciente, mostrara sus datos
+    cronometro_tab: QWidget  # Una vez seleccionado paciente, lo testara
+    rendimiento_tab: QWidget  # Una vez seleccionado paciente, mostrara su rendimiento
+
+    pacients_list_view: QWidget  # Mostrara los pacientes en lista
+    actions_buttons: dict  # Continene los botones de la app
 
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
@@ -41,47 +45,43 @@ class ChronometerWidget(QWidget):
         buttons_layout = QHBoxLayout()
         content_layout = QHBoxLayout()
 
-        actions_buttons = {
+        self.actions_buttons = {
             'add': QPushButton('Anadir'),
             'delete': QPushButton('Eliminar'),
             'edit': QPushButton('Editar')
         }
 
-        actions_buttons['add'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        actions_buttons['delete'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        actions_buttons['edit'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.actions_buttons['add'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.actions_buttons['delete'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.actions_buttons['edit'].setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
-        buttons_layout.addWidget(actions_buttons['add'])
-        buttons_layout.addWidget(actions_buttons['delete'])
-        buttons_layout.addWidget(actions_buttons['edit'])
-        buttons_layout.setAlignment(Qt.AlignLeft )
+        buttons_layout.addWidget(self.actions_buttons['add'])
+        buttons_layout.addWidget(self.actions_buttons['delete'])
+        buttons_layout.addWidget(self.actions_buttons['edit'])
+        buttons_layout.setAlignment(Qt.AlignLeft)
         buttons_layout.setSpacing(20)
 
-        green_rectangle = Color("green")
+        self.pacients_list_view = Color("green")
 
-        parent_tab_widget = QTabWidget()
-        tab_patient = Color("red")  #Tab1 Color
+        self.parent_tab_widget = QTabWidget()
+        self.pacients_tab = Color("red")  # Tab1 Color
 
-        tab_performance = MplCanvas(self, width=5, height=4, dpi=100)   #Tab2 Grafica
-        tab_performance.axes.plot([0,1,2,3,4], [10,1,20,3,40])  
+        self.rendimiento_tab = MplCanvas(self, width=5, height=4, dpi=100)  # Tab2 Grafica
+        self.rendimiento_tab.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
 
-        tab_chrono = Color("green") #Tab3 Color
+        self.cronometro_tab = Color("green")  # Tab3 Color
 
-        parent_tab_widget.resize(300, 300)  #Tab Parent
+        self.parent_tab_widget.resize(300, 300)  # Tab Parent
 
+        self.parent_tab_widget.addTab(self.pacients_tab, "Paciente")
+        self.parent_tab_widget.addTab(self.rendimiento_tab, "Rendimiento")
+        self.parent_tab_widget.addTab(self.cronometro_tab, "Cronómetro")
 
+        self.pacients_list_view.setMinimumSize(200, 400)
+        self.pacients_list_view.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
 
-        parent_tab_widget.addTab(tab_patient, "Paciente")
-        parent_tab_widget.addTab(tab_performance, "Rendimiento")
-        parent_tab_widget.addTab(tab_chrono, "Cronómetro")
-
-    
-        green_rectangle.setMinimumSize(200, 400)
-        green_rectangle.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-
-        content_layout.addWidget(green_rectangle, stretch=3, alignment=Qt.AlignTop)
-        content_layout.addWidget(parent_tab_widget, stretch=9)
-        
+        content_layout.addWidget(self.pacients_list_view, stretch=3, alignment=Qt.AlignTop)
+        content_layout.addWidget(self.parent_tab_widget, stretch=9)
 
         general_layout.addLayout(buttons_layout)
         general_layout.addLayout(content_layout)
